f7d0d6e674d8b09025bdad6654ae44bf395960c0
Karissa McKendall <kmckendall@gmail.com>
2018-01-31 21:14:05 -0500

    added code linking everything

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var tableData = require("../data/friends");
+
+module.exports = function (app) {
+    
+    app.get("/api/friends", function (req, res) {
+        res.json(friend);
+    });
+
+    app.post("/api/friends", function (req, res) {
+      
+    });   
+};
