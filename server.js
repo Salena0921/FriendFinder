e350d9a52650761ef88b53e74dde5704308eda25
Karissa McKendall <kmckendall@gmail.com>
2018-07-09 18:39:50 -0400

    updated file structure

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
